9d710b3dc9d53ddb065c45e395cd2c23cd561ff6
Julian Dickens <jdicken2@hawk.iit.edu>
2017-11-27 22:50:04 -0600

    Changed routes and static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
   get  '/login',        to: 'sessions#new'
   post  '/login',      to: 'sessions#create'
   delete '/logout',    to: 'sessions#destroy'
+
+  root 'welcome#homepage'
+  get  'welcome/homepage'
+  get  'welcome/loginpage'
+  get  'welcome/aboutpage'
+  get  'welcome/signuppage'
   resources :users
 end
